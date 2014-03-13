844cb9d203e3acaeeb03188a8d8f326728eb40e6
gcorcoran <gcorcoran@my.wctc.edu>
2014-03-12 21:47:08 -0500

    Started working on 2nd lab

---
+++ src/lab2/DateService.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab2;
+
+import java.util.Date;
+
+/**
+ *
+ * @author gcorcoran
+ */
+public class DateService {
+    /*
+    Create a class “DateService” and one method that converts a String
+    representation of a date to a Date object. The method should accept a date
+    String in the format “3/15/2014”. Next, create another method in the class that
+    accepts a date String in the format “March 15, 2014 2:30 PM” and converts it
+    to a Calendar object. Test both methods by creating a Startup class and
+    outputting the results
+    */
+    public Date convertStringToDate(String string){
+        String tempString = string;
+        
+        
+        
+        Date temp = new Date();
+        return temp;
+    }
+}
